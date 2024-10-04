d52e3ed20b8562620ff5528e849c54f67376e821
gsusgit <fernandezjodar.jesus@gmail.com>
2024-10-04 11:09:27 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Vue Embed Application
+
+This project is a simple Vue application embedded in HTML. It features a counter and a goal tracker, allowing users to add and manage their goals.
+
+## Features
+
+- **Counter**: Increment and decrement a counter with alerts for maximum limits.
+- **Goals Management**: Add, check for duplicates, and reset goals.
+- **Loading State**: Displays a loading message for 1.5 seconds on startup.
+
+## Technologies Used
+
+- Vue.js
+
+## Installation
+
+1. Clone the repository:
+   ```bash
+   git clone https://github.com/yourusername/vue-embed.git
---
+++ app.js
@@ -0,0 +1,64 @@
+Vue.createApp({
+  data() {
+    return {
+      loading: true,
+      message: 'Hello world!',
+      counter: 0,
+      showAlert: false,
+      alertMessage: '',
+      goal: '',
+      goals: []
+    }
+  },
+  mounted() {
+    setTimeout(() => {
+      this.loading = false
+    }, 1500)
+  },
+  computed: {
+    counterIsZero() {
+      return this.counter === 0
+    },
+    zeroGoals() {
+      return this.goals.length === 0
+    }
+  },
+  watch: {
+    counter(value) {
+      if (value >= 10) {
+        this.showAlert = true
+        this.alertMessage = 'Max counter reached, reseting...'
+        setTimeout(() => {
+          this.counter = 0
+          this.showAlert = false
+        }, 3000)
+      } else {
+        this.showAlert = false
+      }
+    }
+  },
+  methods: {
+    add() {
+      this.counter++
+    },
+    remove() {
+      if (this.counter > 0) this.counter--
+    },
+    newGoal() {
+      if (this.goals.find((goal) => goal === this.goal)) {
+        this.showAlert = true
+        this.alertMessage = 'Goal already exists'
+        setTimeout(() => {
+          this.showAlert = false
+        }, 3000)
+      } else {
+        this.goals.push(this.goal)
+        this.goal = ''
+      }
+    },
+    resetGoals() {
+      if(window.confirm('Are you sure you want to reset goals?'))
+      this.goals = []
+    }
+  }
+}).mount('#app')
